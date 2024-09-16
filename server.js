2af9959ea71a38e314b4fc2b6488648bb8866775
bz2006 <bezelelb260@gmail.com>
2024-09-17 01:08:26 +0530

    debug

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'vlt-hub', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log('****Server Started on '+" PORT:"+ PORT+"****")
 })
